34e65d8e04cf448566de0a81b1904667c2496878
Spryzhen <mathis.stefanini@epita.fr>
2026-04-28 13:53:07 +0300

    read_credential function added and rename of some functions

--- src/wallet_data_functions.py
+++ src/wallet_data_functions.py
@@ -23,7 +23,7 @@ def get_index_element(name=str, app=str) -> int:
     return -1
 
 
-def add_app_to_wallet(name=str, app=str, username=str, password=str) -> None:
+def add_credential_to_wallet(name=str, app=str, username=str, password=str) -> None:
     """
     Function that adds credentials to the user's wallet.\n
     Takes the user's local name, the application name, the username used and the password that he wants to store.
@@ -44,7 +44,7 @@ def add_app_to_wallet(name=str, app=str, username=str, password=str) -> None:
         json.dump(file_data, f, indent=4)
 
 
-def del_app_from_wallet(name=str, app=str) -> int:
+def del_credential_from_wallet(name=str, app=str) -> int:
     """
     Function that deletes credential information of the application given from the user's .json file.\n
     Returns 0 if the information was deleted successfully and -1 if the application is not in the user's wallet.
@@ -85,9 +85,9 @@ def list_all_applications(name=str) -> None:
             print(e["application"])
 
 
-def edit_credentials(name=str, app=str, new_app_name=str, new_username=str, new_password=str) -> int:
+def edit_credential(name=str, app=str, new_app_name=str, new_username=str, new_password=str) -> int:
     """
-    Function that allows the user to edit existing credentials stored in his wallet.\n
+    Function that allows the user to edit an existing credential stored in his wallet.\n
     Returns 0 if the changes have been applied and returns -1 if the given application is not in the wallet.
     """
 
@@ -99,15 +99,35 @@ def edit_credentials(name=str, app=str, new_app_name=str, new_username=str, new_
     index_app = get_index_element(name, app)
 
     if index_app != -1:
-        credentials = file_data[f"Wallet of {name}"][index_app]
+        credential = file_data[f"Wallet of {name}"][index_app]
 
-        credentials["application"] = new_app_name
-        credentials["username/email"] = new_username
-        credentials["password"] = new_password
+        credential["application"] = new_app_name
+        credential["username/email"] = new_username
+        credential["password"] = new_password
 
         with open(file, "w") as f:
             json.dump(file_data, f, indent=4)
         
+        return 0
+    
+    return -1
+
+def read_credential(name=str, app=str) -> int:
+    """
+    Prints all the information about a stored credential from the given user's .json file.\n
+    Returns 0 if the credential was found, if not, returns -1.
+    """
+    file = file_src.parent / "wallets" / f"{name}_wallet.json"
+
+    with open(file, "r") as f:
+        file_data = json.load(f)
+    
+    index_app = get_index_element(name, app)
+
+    credential = file_data[f"Wallet of {name}"][index_app]
+
+    if index_app != -1:
+        print(f"Application/Website Name : {credential["application"]}\nUsername/Email Address : {credential["username/email"]}\nPassword : {credential["password"]}")
         return 0
     
     return -1
